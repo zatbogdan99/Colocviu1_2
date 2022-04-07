f1411cd7c67d89df3bcba4c51a6732014d1795ac
Bogdan Zat <zatbogdan99@gmail.com>
2022-04-07 11:13:32 +0300

    B and C

--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.java
@@ -1,14 +1,80 @@
 package ro.pub.cs.systems.eim.colocviu1_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Colocviu1_2MainActivity extends AppCompatActivity {
+
+    Button add;
+    Button compute;
+    EditText editText;
+    TextView textView;
+    int suma = 0;
+
+    private final ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.add) {
+                if (editText.getText().toString().matches("[0-9]+")) {
+                    textView.setText(textView.getText().toString() + " + " + editText.getText().toString());
+                }
+            } else if (v.getId() == R.id.compute) {
+                Intent intent = new Intent(getApplicationContext(), Colocviu1_2SecondaryActivity.class);
+                intent.putExtra("total", textView.getText().toString());
+                startActivityForResult(intent, 10);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_2_main);
+
+        add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(buttonClickListener);
+        compute = (Button) findViewById(R.id.compute);
+        compute.setOnClickListener(buttonClickListener);
+        editText = (EditText) findViewById(R.id.nextTerm);
+        textView = (TextView) findViewById(R.id.textView);
+
+//        Intent intent = getIntent();
+//        if (intent != null && intent.getExtras().containsKey("total2")) {
+//            int total = intent.getExtras().getInt("total2");
+//            Toast.makeText(this, "Sum is " + total, Toast.LENGTH_LONG).show();
+//        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("suma", textView.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey("suma")) {
+            textView.setText(savedInstanceState.getString("suma"));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 10) {
+//            suma = intent.getExtras().getInt("total");
+            Toast.makeText(this, "Suma este : " + intent.getExtras().getInt("total"), Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2SecondaryActivity.java
@@ -0,0 +1,47 @@
+package ro.pub.cs.systems.eim.colocviu1_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Colocviu1_2SecondaryActivity extends AppCompatActivity {
+
+    public int findSum(String str)
+    {
+        String temp = "0";
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isDigit(ch))
+                temp += ch;
+            else {
+                sum += Integer.parseInt(temp);
+                temp = "0";
+            }
+        }
+        return sum + Integer.parseInt(temp);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_colocviu12_secondary);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.getExtras().containsKey("total")) {
+            String total = intent.getExtras().getString("total");
+            int sum = findSum(total);
+            intent.putExtra("total", sum);
+            setResult(RESULT_OK, intent);
+            finish();
+
+
+//            Intent intent2 = new Intent(getApplicationContext(), Colocviu1_2MainActivity.class);
+//            intent.putExtra("total2", sum);
+//            startActivityForResult(intent, 10);
+        }
+    }
+}
